fe3bd1a6b819cb6d1359eab061078f9c18cccf88
Víctor Alonso Álvarez <victor.alonso.alvarez.86@gmail.com>
2024-11-27 01:12:28 +0100

    feat: exercise two completed

---
+++ exercise-two/script.js
@@ -0,0 +1,43 @@
+function render(html) {
+  const resultBox = document.getElementById("EXERCISE_RESULT");
+  resultBox.innerHTML += html;
+}
+
+/**
+ * 1. Solicito un número al usario y lo guardo en una variable
+ * 2. Opero con el número dado y lo guardo en otra variable
+ * 3. Compruebo que el valor dado es numérico
+ *    A Si es número, pinto el valor proporcionado por el usuario y el resultado operado
+ *    B Si no es número, pido amablemente repetición
+ */
+
+const firstValue = +prompt("Introduzca unos graditos Celsius, que yo se los convierto", "0"); // paso 1
+const secondValue = (firstValue * 9 / 5) + 32; // paso 2
+
+if (Number(firstValue) % 1 === 0)  { // paso 3
+    // es número: paso 3A
+    render(`
+      <style>
+          .result__text {
+             color: #333;
+             font-size: 24px;
+          }
+          .result__text span.celsius{
+             color: red;
+          }
+          .result__text span.farenheit{
+             color: red;
+          }
+      </style>
+  
+      <p class="result__text">
+          Tus graditos Celsius: <span class="celsius">${firstValue}ºC</span> 
+      </p>
+      <p class="result__text">
+          Han resultado ser: <span class="farenheit">${secondValue}ºF</span> grados Farenheit 
+      </p>
+  `); 
+} else {
+  // no es número: paso 3B
+  render(`<p class="result__text">Eso no eran graditos, por favor repite.</p>`);
+} 
